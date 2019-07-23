77dadb0d1df1216ac094bc4ec5ea302338001a2b
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-23 01:02:21 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,6 @@ def meal_choice(broccoli, macaroni, protein="tofu")
   returns "What a nutritious meal!"
   puts "A plate of tofu with #{broccoli} and #{macaroni}."
   returns "What a nutritious meal!"
-  puts "A plate of #{veg1} and #{veg2}."
+  puts "A plate of #{broccoli} and #{macaroni}."
   returns "What a nutritious meal!"
 end
