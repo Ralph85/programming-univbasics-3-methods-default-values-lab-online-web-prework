2fe1122e5a36e4a476d48deac4c5289241366ab6
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-23 00:57:41 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,5 @@
-def meal_choice(broccoli, macaroni, tofu="meat")
-  puts "A plate of meat with #{broccoli} and #{macaroni}"
+def meal_choice(broccoli, macaroni, protein="tofu")
+  puts "A plate of meat with #{broccoli} and #{macaroni}."
   puts "What a nutritious meal!"
+  puts "A plate of tofu with #{broccoli} and #{macaroni}."
 end
