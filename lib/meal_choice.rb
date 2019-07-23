00a3e660cff5d1babf653d5cfc0c6d183fb4ef24
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-23 00:42:31 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,5 +2,6 @@ def meal_choice(veg1, veg2, protein="meat")
   if "A plate of #{protein} with #{veg1} and #{veg2}."
     puts "What a nutritious meal!"
   end
+end
 
 meal_choice(veg1,  veg1, protein="meat")
